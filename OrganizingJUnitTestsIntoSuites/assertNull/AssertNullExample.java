c23d83930bc8ff66d76dad0ad20ea5438a9dbdb3
jamal <jamalkacade@gmail.com>
2024-12-25 21:16:13 +0800

    pass null value as string and fails and again pass as null to the name and the model instances in the Car class

--- OrganizingJUnitTestsIntoSuites/assertNull/AssertNullExample.java
+++ OrganizingJUnitTestsIntoSuites/assertNull/AssertNullExample.java
@@ -3,8 +3,11 @@ package OrganizingJUnitTestsIntoSuites.assertNull;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
+import  OrganizingJUnitTestsIntoSuites.Car;
+
 public class AssertNullExample {
 
+    Car car = new Car();
     @Test
     public void testNull() {
         String str = null;
@@ -23,4 +26,30 @@ public class AssertNullExample {
         // is used to check if a value is null
         
     }
+
+    @Test
+    public void testAssertNull3() {
+        car.setName("null");
+        car.setModel("null");
+
+        assertNull(car.getName());// fails because name is not null it has a value which is not null as string
+        assertNull(car.getModel());// fails because name is not null it has a value which is not null as string
+        // this method fails if the name is not null
+        // this method passes if the name is null
+        // is used to check if a value is null
+        
+    }
+
+    @Test
+    public void testAssertNull4() {
+        car.setName(null); // Set name to null
+        car.setModel(null); // Set model to null
+
+        assertNull(car.getName());  // This will pass because name is null
+        assertNull(car.getModel()); // This will pass because model is null
+    }
+
+    
+
+
 }
